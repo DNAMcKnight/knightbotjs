55d89cd4f724a7de53e0cf41079d22f1e9bd442d
DNAMcKnight <conner.cmk9040@gmail.com>
2024-11-09 17:52:03 +0600

    updated formatting.

--- src/handlers/SlashHandlers.js
+++ src/handlers/SlashHandlers.js
@@ -1,21 +1,13 @@
 import { PingCommand } from "../commands/core.js";
-import { SyncCommand } from "../commands/sync.js";
-import { UnsyncCommand } from "../commands/unsync.js";
 
-export const slashCommands = [
-  PingCommand,
-  SyncCommand,
-  UnsyncCommand
-];
-
-
+export const slashCommands = [PingCommand];
 
 export const handleSlashCommand = async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  for (const cmd of slashCommands) {
-    if (interaction.commandName === cmd.data.name) {
-      await cmd.run(interaction);
+  for (const command of slashCommands) {
+    if (interaction.commandName === command.data.name) {
+      await command.run(interaction);
     }
   }
 };
--- src/scripts/sync_commands.js
+++ src/scripts/sync_commands.js
@@ -3,38 +3,34 @@ import { REST, Routes } from "discord.js";
 config();
 
 const commands = [
-    {
-        name:'ping',
-        description: "Replies with pong!"
-    }
+  {
+    name: "ping",
+    description: "Replies with pong!",
+  },
 ];
 
-const rest = new REST({ version: "10"}).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-    try {
-        console.log("Registering slash commands...")
-        if (process.env.DEBUG === "1") {
-            console.log("Registering Guild slash commands...")
-            await rest.put(
-                Routes.applicationGuildCommands(
-                    process.env.CLIENT_ID,
-                    process.env.GUILD_ID
-                ),
-                {body: commands}
-            );
-        } else {
-            await rest.put(
-                Routes.applicationGuildCommands(
-                    process.env.CLIENT_ID,
-                ),
-                {body: commands}
-            );
-        }
-        
-    console.log("Command registerd successfully!")
-    } catch (error) {
-        console.log(`There was an error: ${error}`)
+  try {
+    console.log("Registering slash commands...");
+    if (process.env.DEBUG === "1") {
+      console.log("Registering Guild slash commands...");
+      await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID,
+          process.env.GUILD_ID
+        ),
+        { body: commands }
+      );
+    } else {
+      await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID), {
+        body: commands,
+      });
     }
-})();
 
+    console.log("Command registerd successfully!");
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
