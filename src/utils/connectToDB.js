4967e63431e74f3ab73ccf1df5f2b1c58a71c141
DNAMcKnight <conner.cmk9040@gmail.com>
2024-12-10 02:52:54 +0600

    added more database calls

--- src/utils/connectToDB.js
+++ src/utils/connectToDB.js
@@ -13,15 +13,11 @@ export const getLinkedTags = async (discordID) => {
     await client.connect();
 
     // Make the appropriate DB calls
-    const cursor = client
-      .db("Database")
-      .collection("account_data")
-      .find(
-        {
-          discord_id: discordID,
-        },
-        { projection: { _id: 1 } }
-      );
+    const accounts = client.db("Database").collection("account_data");
+    const cursor = await accounts.find(
+      { discord_id: discordID },
+      { projection: { _id: 1 } }
+    );
     const result = await cursor.toArray();
     return result.map((doc) => doc._id);
   } catch (e) {
@@ -41,13 +37,11 @@ export const getLinkedUser = async (tag) => {
 
     await client.connect();
 
-    const cursor = await client
-      .db("Database")
-      .collection("account_data")
-      .findOne(
-        { _id: tag },
-        { projection: { discord_id: 1, _id: 0 } } // Only return discord_id field
-      );
+    const accounts = client.db("Database").collection("account_data");
+    const cursor = await accounts.findOne(
+      { _id: tag },
+      { projection: { discord_id: 1, _id: 0 } } // Only return discord_id field
+    );
 
     return cursor?.discord_id; // Returns just the discord_id value
   } catch (e) {
@@ -58,36 +52,50 @@ export const getLinkedUser = async (tag) => {
   }
 };
 
-// console.log(await getLinkedTags("310517079642079234"));
-// async function main() {
-//   const client = new MongoClient(DB_URL);
+export const linkUser = async (resp, discord_id) => {
+  const client = new MongoClient(DB_URL);
 
-//   try {
-//     // Connect to the MongoDB cluster
-//     await client.connect();
+  try {
+    if (!tag.startsWith("#")) {
+      tag = `#${tag}`;
+    }
 
-//     // Make the appropriate DB calls
-//     const cursor = await client.db("Database").collection("account_data").find({
-//       _id: "#PY89PLLY",
-//     });
-//     console.log("Databases:");
-//     const result = await cursor.toArray();
-//     console.log(result);
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
+    await client.connect();
+    const date = new Date().toISOString().replace(/\.\d{3}Z$/, ".000+00:00");
+    const accounts = client.db("Database").collection("account_data");
+    const cursor = await accounts.insertOne({
+      _id: resp.tag,
+      discord_id: String(discord_id),
+      player_info: resp,
+      last_update: date,
+    });
 
-// async function listDatabases(client) {
-//   // const databasesList = await client.db().admin().listDatabases();
-//   const cursor = await client.db("Database").collection("account_data").find({
-//     _id: "#PY89PLLY",
-//   });
-//   console.log("Databases:");
-//   const result = await cursor.toArray();
-//   console.log(result);
-// }
+    return true;
+  } catch (e) {
+    console.error(e);
+    return null;
+  } finally {
+    await client.close();
+  }
+};
 
-// main().catch(console.error);
+export const unlinkUser = async (tag) => {
+  const client = new MongoClient(DB_URL);
+
+  try {
+    if (!tag.startsWith("#")) {
+      tag = `#${tag}`;
+    }
+
+    await client.connect();
+    const accounts = client.db("Database").collection("account_data");
+    const cursor = await accounts.deleteOne({ _id: tag });
+
+    return true;
+  } catch (e) {
+    console.error(e);
+    return null;
+  } finally {
+    await client.close();
+  }
+};
