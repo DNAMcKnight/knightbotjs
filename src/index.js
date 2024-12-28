0531c6ab454a4774cf11f1cd7381f73db7fc9bfe
DNAMcKnight <conner.cmk9040@gmail.com>
2024-12-28 07:47:48 +0600

    we're now syncing and loading commands in a loop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { config } from "dotenv";
-import { Client, IntentsBitField } from "discord.js";
-import { handleSlashCommand } from "./handlers/SlashHandlers.js";
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  IntentsBitField,
+} from "discord.js";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 config();
 
 const token = process.env.TOKEN;
 const client = new Client({
   intents: [
+    GatewayIntentBits.Guilds,
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
@@ -14,20 +24,50 @@ const client = new Client({
   ],
 });
 
-client.on("ready", (ctx) => {
-  console.log(`${ctx.user.tag} is ready!`);
-});
+client.commands = new Collection();
 
-client.on("interactionCreate", handleSlashCommand);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-// client.on("messageCreate", (message) => {
-//   if (message.content === "hello") {
-//     message.reply("Hey!");
-//   }
-// });
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command.default && "execute" in command.default) {
+      client.commands.set(command.default.data.name, command.default);
+      if (process.env.DEBUG === "1") {
+        console.log(`${command.default.data.name} command loaded.`);
+      } else {
+        console.log(process.env.DEBUG);
+      }
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
-process.on("uncaughtException", (error) => {
-  console.log(error.stack);
-});
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = await import(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(token);
--- src/scripts/sync_commands.js
+++ src/scripts/sync_commands.js
@@ -1,40 +1,75 @@
-import { config } from "dotenv";
 import { REST, Routes } from "discord.js";
+import { config } from "dotenv";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+
 config();
 
-const commands = [
-  {
-    name: "ping",
-    description: "Replies with pong!",
-  },
-  {
-    name: "test",
-    description: "Welcome to test command!",
-  },
-];
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const foldersPath = path.join(__dirname, "../commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(filePath);
+    if ("data" in command.default && "execute" in command.default) {
+      commands.push(command.default.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.TOKEN);
+
+// and deploy your commands!
 (async () => {
   try {
-    console.log("Registering slash commands...");
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
     if (process.env.DEBUG === "1") {
       console.log("Registering Guild slash commands...");
-      await rest.put(
+      const data = await rest.put(
         Routes.applicationGuildCommands(
           process.env.CLIENT_ID,
           process.env.GUILD_ID
         ),
         { body: commands }
       );
+      console.log(
+        `Successfully reloaded local guild ${data.length} application (/) commands.`
+      );
     } else {
-      await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID), {
-        body: commands,
-      });
+      const data = await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID),
+        {
+          body: commands,
+        }
+      );
+      console.log(
+        `Successfully reloaded global guilds ${data.length} application (/) commands.`
+      );
     }
-
-    console.log("Command registerd successfully!");
   } catch (error) {
-    console.log(`There was an error: ${error}`);
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
   }
 })();
